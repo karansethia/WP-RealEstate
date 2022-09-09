fb29313400c8c561c8a0d24dd52bdf84cd493367
Karan <karansethia24@gmail.com>
2022-09-10 04:23:33 +0530

    wp website with ACF

---
+++ wp-content/plugins/astra-pro-sites/inc/lib/onboarding/assets/dist/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '9a5c77bba7cc6cbed2d5c2f6c0401ca2');
